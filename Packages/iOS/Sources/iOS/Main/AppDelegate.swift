3d0a64afec953b3faebd4dbfc9024b7fabfad9f1
Huanan <afuture.d@outlook.com>
2025-07-22 17:46:35 +0800

    init

---
+++ Packages/iOS/Sources/iOS/Main/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by 尼诺 on 2023/12/8.
+//
+
+import Foundation
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        true
+    }
+    
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return .defaultScene
+    }
+}
---
+++ Template/TemplateApp.swift
@@ -0,0 +1,8 @@
+//
+//  TemplateApp.swift
+//  Template
+//
+//  Created by 尼诺 on 2023/12/8.
+//
+
+import Foundation
